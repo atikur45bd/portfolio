439fc85af9333f37dc4383538db65cc02d5ab0b9
Md Atikur Rahman <atikurra00005@gmail.com>
2024-09-13 16:14:53 +0600

    navbar script

---
+++ script.js
@@ -0,0 +1,8 @@
+const showNavBar = () =>{
+    navBar= document.querySelector('.side-bar');
+    navBar.style.display='flex';
+}
+const hideNavBar = () =>{
+    navBar= document.querySelector('.side-bar');
+    navBar.style.display='none';
+}
